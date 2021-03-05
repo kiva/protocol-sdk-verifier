10af4310a10d53bd33a6123811439dce83c5e86a
Nate Sulat <nates@kiva.org>
2021-03-05 15:11:19 -0800

    PRO-2781: Adding Github Action for functional tests

--- test/functional/qr_scan_spec.js
+++ test/functional/qr_scan_spec.js
@@ -1,5 +1,3 @@
-// Beware: route2() is experimental (https://docs.cypress.io/api/commands/route2.html)
-
 let conf;
 let common;
 
@@ -19,9 +17,10 @@ describe('QR Code Scan screen should...', function() {
                 ...common
             };
 
-        cy.route2(testConf.method, testConf.endpoint, response);
+        cy.intercept(testConf.method, testConf.endpoint, response);
         cy.visit('/');
         cy.get('.accept').click();
+        cy.get('#select-auth-method').click();
         cy.get('.dialog-icon.error').should('be.visible');
     });
 
@@ -33,7 +32,7 @@ describe('QR Code Scan screen should...', function() {
                 delayMs,
                 ...common
             };
-        cy.route2(testConf.method, testConf.endpoint, response);
+        cy.intercept(testConf.method, testConf.endpoint, response);
         cy.get('[data-cy="reset-flow"]').click();
         cy.get('#qr-loader').should('be.visible');
         cy.wait(delayMs + 100);
@@ -41,7 +40,7 @@ describe('QR Code Scan screen should...', function() {
 
     it('should go back to the Confirmation screen when "Back" is clicked', function() {
         cy.get('[data-cy="qr-back"]').click();
-        cy.get('#Kiva_QR').should('not.be.visible');
+        cy.get('#Kiva_QR').should('not.exist');
     });
 
     it('should load a QR code when the request is successful', function() {
@@ -50,20 +49,17 @@ describe('QR Code Scan screen should...', function() {
             establishResponseBody = establishConf.success,
             connectResponseBody = getConf.inactive;
 
-        cy.route2(establishConf.method, establishConf.endpoint, {
+        cy.intercept(establishConf.method, establishConf.endpoint, {
             ...common,
             body: establishResponseBody
         });
-        cy.route2(getConf.method, getConf.endpoint, {
+        cy.intercept(getConf.method, getConf.endpoint, {
             ...common,
             body: connectResponseBody
         });
-        cy.get('.accept').click();
+        cy.get('#select-auth-method').click();
         cy.get('#qr-code').should('be.visible');
     });
-
-    // The inner workings of route2() remain somewhat opaque, but this seems to work for clearing a stub from the previous test
-    it('does nothing', function() {});
 
     it('should show an error message when there\'s an error in the status check', function() {
         let establishConf = conf.establishConnection,
@@ -71,13 +67,12 @@ describe('QR Code Scan screen should...', function() {
             establishResponseBody = establishConf.success,
             connectResponseBody = getConf.inactive;
 
-        cy.route2(establishConf.method, establishConf.endpoint, {
+        cy.intercept(establishConf.method, establishConf.endpoint, {
             ...common,
             body: establishResponseBody
         });
-        cy.route2(getConf.method, getConf.endpoint, {
+        cy.intercept(getConf.method, getConf.endpoint, {
             ...common,
-            delayMs: 400,
             statusCode: 418,
             body: connectResponseBody
         });
@@ -92,13 +87,12 @@ describe('QR Code Scan screen should...', function() {
             establishResponseBody = establishConf.success,
             connectResponseBody = getConf.active;
 
-        cy.route2(establishConf.method, establishConf.endpoint, {
+        cy.intercept(establishConf.method, establishConf.endpoint, {
             ...common,
             body: establishResponseBody
         });
-        cy.route2(getConf.method, getConf.endpoint, {
+        cy.intercept(getConf.method, getConf.endpoint, {
             ...common,
-            delayMs: 200,
             body: connectResponseBody
         });
 
@@ -111,11 +105,11 @@ describe('QR Code Scan screen should...', function() {
         let verifyConf = conf.sendVerification,
             checkConf = conf.checkVerification;
 
-        cy.route2(verifyConf.method, verifyConf.endpoint, {
+        cy.intercept(verifyConf.method, verifyConf.endpoint, {
             ...common,
             body: verifyConf.success
         });
-        cy.route2(checkConf.method, checkConf.endpoint, {
+        cy.intercept(checkConf.method, checkConf.endpoint, {
             ...common,
             delayMs: 1000,
             body: checkConf.unverified
@@ -132,11 +126,11 @@ describe('QR Code Scan screen should...', function() {
         let verifyConf = conf.sendVerification,
             checkConf = conf.checkVerification;
 
-        cy.route2(verifyConf.method, verifyConf.endpoint, {
+        cy.intercept(verifyConf.method, verifyConf.endpoint, {
             ...common,
             body: verifyConf.success
         });
-        cy.route2(checkConf.method, checkConf.endpoint, {
+        cy.intercept(checkConf.method, checkConf.endpoint, {
             ...common,
             statusCode: 418
         });
@@ -145,17 +139,15 @@ describe('QR Code Scan screen should...', function() {
         cy.get('.dialog-icon.error').should('be.visible');
     });
 
-    it('does nothing', function() {});
-
     it('should render user details if the proof is sucessful', function() {
         let verifyConf = conf.sendVerification,
             checkConf = conf.checkVerification;
 
-        cy.route2(verifyConf.method, verifyConf.endpoint, {
+        cy.intercept(verifyConf.method, verifyConf.endpoint, {
             ...common,
             body: verifyConf.success
         });
-        cy.route2(checkConf.method, checkConf.endpoint, {
+        cy.intercept(checkConf.method, checkConf.endpoint, {
             ...common,
             body: checkConf.verified
         });
